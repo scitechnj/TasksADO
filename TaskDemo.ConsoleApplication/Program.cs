c8f93c127fe132c8cffb42e7b6ef77c168608b19
Avrumi Friedman <datmonkey2002@yahoo.com>
2013-12-08 11:56:06 -0500

    Initial Commit

---
+++ TaskDemo.ConsoleApplication/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDemo.DataLayer;
+using TaskDemo.Entities;
+using Task = TaskDemo.Entities.Task;
+
+namespace TaskDemo.ConsoleApplication
+{
+    class Program
+    {
+
+        private static TaskDatabaseManager _manager =
+            new TaskDatabaseManager(Settings.Default.ConnectionString);
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello, Hit 1 for Categories Menu, 2 for Tasks Menu");
+                if (Console.ReadLine() == "1")
+                {
+                    ShowCategoriesMenu();
+                }
+                else
+                {
+                    ShowTasksMenu();
+                }
+                Console.WriteLine("Hit q to quit");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+
+        }
+
+        private static void ShowCategoriesMenu()
+        {
+            Console.WriteLine("Press 1 to add a new Category");
+            string response = Console.ReadLine();
+            if (response == "1")
+            {
+                Console.WriteLine("Enter a category name:");
+                string name = Console.ReadLine();
+                Category category = new Category { Name = name };
+                _manager.AddCategory(category);
+                Console.WriteLine("New category Added, Id: " + category.Id);
+            }
+        }
+
+        private static void ShowTasksMenu()
+        {
+            Console.WriteLine("Press 1 to add a task, 2 to delete a task");
+            string response = Console.ReadLine();
+            if (response == "1")
+            {
+                Category theCategory = GetCategory();
+                Console.WriteLine("Enter a task description:");
+                string description = Console.ReadLine();
+                Task task = new Task { Description = description, Category = theCategory };
+                _manager.AddTask(task);
+                Console.WriteLine("Task created, Id: " + task.Id);
+            }
+            else if (response == "2")
+            {
+                Category category = GetCategory();
+                int i = 1;
+                foreach (Task task in category.Tasks)
+                {
+                    Console.WriteLine("{0}. {1}", i, task.Description);
+                    i++;
+                }
+
+                Console.WriteLine("Choose a task to delete from the list above");
+                int selectedId = int.Parse(Console.ReadLine());
+                Task selectedTask = category.Tasks.ElementAt(selectedId - 1);
+                _manager.DeleteTask(selectedTask);
+            }
+        }
+
+        private static Category GetCategory()
+        {
+            IEnumerable<Category> categories = _manager.GetCategories();
+            int i = 1;
+            foreach (Category category in categories)
+            {
+                Console.WriteLine("{0}. {1}", i, category.Name);
+                i++;
+            }
+            Console.WriteLine("Choose a category from the list above");
+            int categoryChosen = int.Parse(Console.ReadLine());
+            Category theCategory = categories.ElementAt(categoryChosen - 1);
+            return theCategory;
+        }
+    }
+}
---
+++ TaskDemo.DataLayer/TaskDatabaseManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using TaskDemo.Entities;
+using Task = TaskDemo.Entities.Task;
+
+
+namespace TaskDemo.DataLayer
+{
+    //public class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        TaskDatabaseManager mgr = new TaskDatabaseManager(@"Data Source=(local)\SQLEXPRESS;Initial Catalog=TaskDemo;Integrated Security=True");
+    //        mgr.AddCategory(new Category { Name = "TestCat" });
+    //    }
+    //}
+
+    public class TaskDatabaseManager
+    {
+        private readonly string _connectionString;
+
+
+        public TaskDatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddCategory(Category category)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Categories VALUES(@name); SELECT @@Identity";
+                command.Parameters.AddWithValue("@name", category.Name);
+                connection.Open();
+                int id = (int)(decimal)command.ExecuteScalar();
+                category.Id = id;
+            }
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = @"SELECT 
+                                        c.id as CategoryId, 
+                                        c.Name as CategoryName, 
+                                        t.Description as TaskDescription, 
+                                        t.CategoryId as TaskCatId, 
+                                        t.Id as TaskID 
+                                        FROM Categories c  
+                                        LEFT JOIN Tasks t ON c.Id=t.CategoryId";
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int catId = int.Parse(reader["CategoryId"].ToString());
+                    Category category = categories.FirstOrDefault(n => n.Id == catId);
+                    if (category == null)
+                    {
+                        category = new Category();
+                        category.Name = (string)reader["CategoryName"];
+                        category.Id = catId;
+                        category.Tasks = new List<Task>();
+                        categories.Add(category);
+                    }
+
+                    if (reader["TaskID"] != DBNull.Value)
+                    {
+                        Task task = new Task()
+                            {
+                                Id = int.Parse(reader["TaskID"].ToString()),
+                                Category = category,
+                                Description = (string)reader["TaskDescription"]
+                            };
+                        List<Task> list = (List<Task>)category.Tasks;
+                        list.Add(task);
+                    }
+
+                    
+                }
+                return categories;
+            }
+        }
+
+        public void AddTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "AddTask";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Description", task.Description);
+                command.Parameters.AddWithValue("@CategoryId", task.Category.Id);
+
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@taskId";
+                parameter.DbType = DbType.Int32;
+                parameter.Direction = ParameterDirection.Output;
+                command.Parameters.Add(parameter);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                task.Id = int.Parse(parameter.Value.ToString());
+            }
+        }
+
+        public void DeleteTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "DELETE FROM Tasks WHERE Tasks.Id = @Id";
+                command.Parameters.AddWithValue("@Id", task.Id);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
